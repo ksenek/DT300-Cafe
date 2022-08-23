06c4fd6e9f68cde9e13eec61ef694b76bd44d672
ksenek <110329390+ksenek@users.noreply.github.com>
2022-08-24 09:09:01 +1200

    validate entries on insert

--- insert.php
+++ insert.php
@@ -5,7 +5,54 @@
         echo "Could not connect to database";
         exit();
     }
+/*
+    PHP Form Validation
+    https://www.w3schools.com/php/php_form_validation.asp
+    */
+    // define variables and set to empty values
+    $drink = $cost = "";
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $drink = test_input($_POST["drink"]);
+        $cost = test_input($_POST["cost"]);
+    }
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    function terminate_script() {
+        /* Refresh the page after 2 seconds and return to the add_drinks.php page */
+        header("refresh:2; url=add_drinks.php");
+        exit();     /* Terminate script */
+    }
+
+
+    /* Check the length of the string */
+    if (strlen($drink)>5) {
+        echo 'Drink Name too long';
+        terminate_script();
+    }
+
+    /*
+    Validating the cost
+    https://supunkavinda.blog/php/input-validation-with-php#user-inputs
+    */
+    if (!empty($cost)) {
+
+        $number = filter_var($cost, FILTER_VALIDATE_INT);
+
+        /* Checks the number is a number and is within range */
+        if ($number === false or $cost < 0 or $cost > 99) {
+            echo 'Invalid Integer' ;
+            terminate_script();
+        }
+
+    }
+	
     /* Insert into drinks table Query */
     $drink = $_POST['drink'];
     $cost = $_POST['cost'];
